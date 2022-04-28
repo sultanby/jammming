5373efa0e755c26c3d4943a94188ef56a87e9928
sultanby <jemjasott@gmail.com>
2022-04-28 13:38:02 +0200

    changing the name of a playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -31,6 +32,10 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks})
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
   render() { 
     return (
       <div>
@@ -46,6 +51,7 @@ class App extends React.Component {
             playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks}
             onRemove={this.removeTrack}
+            onChangeName={this.updatePlaylistName}
           />
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,17 @@ import "./Playlist.css";
 import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  handleNameChange(e){
+    this.props.onChangeName(e.target.value)
+  }
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue='New Playlist' />
+        <input defaultValue='New Playlist' value={this.props.playlistName} onChange={this.handleNameChange}/>
         <TrackList 
             tracks={this.props.playlistTracks}
             onRemove={this.props.onRemove}
