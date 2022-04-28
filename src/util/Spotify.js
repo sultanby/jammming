191849c21ea08034005a75b649bdd0c36445e6f8
sultanby <jemjasott@gmail.com>
2022-04-28 18:08:09 +0200

    obtaining a spotify access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,6 @@
+const CLIENT_ID='cbddd3acba224a4ab545becd79fce2b0';
+const REDIRECT_URI='http://localhost:3000/'
+
 let accessToken;
 
 
@@ -5,6 +8,20 @@ let Spotify = {
     getAccessToken(){
         if (accessToken) {
             return accessToken;
+        }
+        const urlToParse = window.location.href;
+        let parsedToken = urlToParse.match(/access_token=([^&]*)/);
+        let parsedExpirationTime = urlToParse.match(/expires_in=([^&]*)/);
+
+        if (parsedToken && parsedExpirationTime) {
+            accessToken = parsedToken[1];
+            let expirationTime = parsedExpirationTime[1];
+            window.setTimeout(() => accessToken = '', expirationTime * 1000);
+            window.history.pushState('Access Token', null, '/');
+        }
+
+        if (!parsedToken) {
+            window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
         }
     }
 }
